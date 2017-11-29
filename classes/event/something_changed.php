aabd0375809cfbadbc96425e3e58f193ebd82eed
Mohammed Essaid MEZERREG <mohammed.tr@zadgroup.net>
2017-11-29 22:04:19 +0100

    Add and event example

---
+++ classes/event/something_changed.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @package  moodle_local_plugin
+ * @copyright 2017, Mohammed Essaid MEZERREG <me@mohessaid.com>
+ * @license MIT
+ * @doc https://docs.moodle.org/dev/Event_2
+ */
+
+namespace moodle_local_plugin\event;
+
+defined('MOODLE_INTERNAL') || die();
+
+
+class something_changed extends base {
+
+}
